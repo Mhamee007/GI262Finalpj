3729fba9fd17d7bcf90480b0e6fd02f8934c4201
NapatBoonna <napat.boonn@bumail.net>
2025-11-13 12:06:49 +0700

    Revert "AT 1"
    This reverts commit 9eda9ccb52a11e41386c06800f7bf6465c30c0aa.

---
+++ Assets/codeBoat/Equipment.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    string name;
+    string description;
+    float pricee;
+
+}
---
+++ Assets/codeBoat/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 20f;
+    public float maxSpeed = 10f;
+    Rigidbody2D rd;
+
+    public GameObject bobber;
+
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        
+        rd.linearVelocity = new Vector2(movement * speed, rd.linearVelocityY);
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+}
