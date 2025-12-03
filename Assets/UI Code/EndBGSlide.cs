8b2a1572b004d96c4f87476bbde823ac54730e85
NapatBoonna <napat.boonn@bumail.net>
2025-12-03 19:42:41 +0700

    End Credit 40%(?)

---
+++ Assets/UI Code/EndBGSlide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EndBGSlide : MonoBehaviour
+{
+    public float moveSpeed = 2f;       // ความเร็ว
+    public Vector3 endPosition;        // จุดที่ต้องการให้ BG ไปถึง
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            endPosition,
+            moveSpeed * Time.deltaTime
+        );
+    }
+}
+
